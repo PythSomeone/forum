a2de25942aefe1ae92ca2e30560414c8a837d381
Huszan <mateuszjacenty1@gmail.com>
2020-05-27 12:47:20 +0200

    MVC

---
+++ ForumV5/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using ForumV5.Models;
+
+namespace ForumV5.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Authorise(ForumV5.Models.User userModel)
+        {
+            using(ForumDBEntities db = new ForumDBEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if(userDetails == null) 
+                {
+                    userModel.LoginErrorMessage = "Fuck off";
+                    return View("Index",userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    Session["userName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
